b9067ea98905c1207b241ba1077a9dc178375ec1
Guillaume Thomas <guillaume.thomas642@gmail.com>
2014-11-13 22:20:42 +0100

    Handle requests_futures

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name = 'django-requests-panel',
-      version = '0.1.0',
-      author = 'Guillaume Thomas',
-      author_email='guillaume.thomas642@gmail.com',
-      license='LICENCE.txt',
-      description='',
-      url = 'https://github.com/gtnx/django-requests-panel',
-      install_requires = map(lambda line:line.strip("\n"), open('requirements.txt', 'r').readlines()),
-      include_package_data = True,
-      packages = find_packages(),
-      )
+setup(
+  name = 'django-requests-panel',
+  version = '0.2.0',
+  author = 'Guillaume Thomas',
+  author_email='guillaume.thomas642@gmail.com',
+  license='LICENCE.txt',
+  description='',
+  url = 'https://github.com/gtnx/django-requests-panel',
+  install_requires = map(lambda line:line.strip("\n"), open('requirements.txt', 'r').readlines()),
+  include_package_data = True,
+  packages = find_packages(),
+  )
